5b1f6b0f731b51fbf483e01f38cd427b72ec4bcd
aokrushko <alex.okrushko@gmail.com>
2018-12-09 22:40:25 -0500

    simpliest metadata sync

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,34 @@ import {
 } from '@angular/material';
 import { ProductDetailsModule } from './product-details/product-details.module';
 import { CartDetailsModule } from './cart-details/cart-details.module';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, ActionReducer, INIT, UPDATE } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { reducer } from './reducer';
+import { reducer as productReducer, GlobalState } from './reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { ProductEffects } from './effects';
 import { RatingModule } from './rating/rating.module';
+
+export function productSync(reducer: ActionReducer<GlobalState>) {
+  return (state, action) => {
+    let reducedState = reducer(state, action);
+    if (action.type === INIT) {
+      const data = window.localStorage.getItem('productData');
+      if (data) {
+        reducedState = {
+          ...reducedState,
+          product: JSON.parse(data),
+        };
+      }
+    }
+    if (action.type !== UPDATE) {
+      window.localStorage.setItem(
+        'productData',
+        JSON.stringify(reducedState.product)
+      );
+    }
+    return reducedState;
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -31,7 +53,10 @@ import { RatingModule } from './rating/rating.module';
     MatIconModule,
     MatToolbarModule,
     MatSnackBarModule,
-    StoreModule.forRoot({ product: reducer }),
+    StoreModule.forRoot(
+      { product: productReducer },
+      { metaReducers: [productSync] }
+    ),
     EffectsModule.forRoot([ProductEffects]),
     StoreDevtoolsModule.instrument({ maxAge: 50 }),
     RatingModule,
